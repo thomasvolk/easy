043e12cc1ea4501f17a8034007b1f29d747d22eb
Thomas Volk (XPS-L502) <thomas@my-volk.de>
2014-08-02 10:53:29 +0200

    ajp for embedded tomcat

--- web/src/main/scala/de/thomasvolk/easy/web/EasyServer.scala
+++ web/src/main/scala/de/thomasvolk/easy/web/EasyServer.scala
@@ -2,12 +2,22 @@ package de.thomasvolk.easy.web
 
 import org.apache.catalina.startup.Tomcat
 import java.io.File
+import org.apache.catalina.connector.Connector
 
 object EasyServer {
   def main(args: Array[String]) {
     val webappDirLocation = "web/src/main/webapp"
     val port = sys.env.getOrElse("EASY_PORT", "8080").toInt
     val tomcat = new Tomcat
+    val ajpConnector = new Connector("org.apache.coyote.ajp.AjpProtocol")
+    ajpConnector.setPort(8009)
+    ajpConnector.setProtocol("AJP/1.3")
+    ajpConnector.setRedirectPort(8443)
+    ajpConnector.setEnableLookups(false)
+    ajpConnector.setProperty("redirectPort", "8443")
+    ajpConnector.setProperty("protocol", "AJP/1.3")
+    ajpConnector.setProperty("enableLookups", "false")
+    tomcat.getService.addConnector(ajpConnector)
     tomcat.setPort(port)
     tomcat.addWebapp("/easy", new File(webappDirLocation).getAbsolutePath())
     tomcat.start()
